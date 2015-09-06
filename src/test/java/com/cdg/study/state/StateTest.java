e0d60cefeecc93566d7da617354b891114fe689f
kanghoon.choi <kanghoon.choi@nhnent.com>
2015-09-06 11:29:09 +0900

    #상태 짐볼머신 최초버전

---
+++ src/test/java/com/cdg/study/state/StateTest.java
@@ -0,0 +1,45 @@
+package com.cdg.study.state;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StateTest {
+
+	@Test
+	public void test() throws Exception {
+		GumballMachine gumballMachine = new GumballMachine(5);
+
+		System.out.println(gumballMachine);
+
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+
+		System.out.println(gumballMachine);
+
+		gumballMachine.insertQuarter();
+		gumballMachine.ejectQuarter();
+		gumballMachine.turnCrank();
+
+		System.out.println(gumballMachine);
+
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.ejectQuarter();
+
+		System.out.println(gumballMachine);
+
+		gumballMachine.insertQuarter();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+
+		System.out.println(gumballMachine);
+
+	}
+}
